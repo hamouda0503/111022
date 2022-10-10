37606398578edd5bdc8fc8153959509ef42c0441
Dridi Ahmed <ahmed.dridi04@gmail.com>
2022-10-10 22:21:36 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Elyoum",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "3malna 35 mois ",  // 同上...
+        "M3a b3adhna",
+        "w nheb in9olik",
+        "eli ena ferhan b wjoudik",
+        "M3aya w bjanbi",
+        "Nhebek Nhebek Nhebek",
+        "na3ch9ek na3ch9ek na3ch9ek",
+        "inchallah nhar el youm yet3adelik 3sal",
+        // "And Then Take It Away",
+        // "YAMI~~",
+    ],
+    wish: "El Khawefa El Kobbi",  //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    time: "2022-10-10 24:0:0",    //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
